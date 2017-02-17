5496b152639b29357f406d29ad21ba091163b325
VINICIUS PEREIRA SEVERO <RM76743@fiap.com.br>
2017-02-17 09:40:06 -0200

    Efetuado CRUD

--- 02-JPA-Introducao/src/br/com/fiap/entity/Aluno.java
+++ 02-JPA-Introducao/src/br/com/fiap/entity/Aluno.java
@@ -1,5 +1,6 @@
 package br.com.fiap.entity;
 
+import java.util.Arrays;
 import java.util.Calendar;
 
 import javax.persistence.Column;
@@ -39,5 +40,79 @@ public class Aluno {
 	
 	@Transient //não persiste no banco
 	private int senhaAtendimento;
+
+	public int getRm() {
+		return rm;
+	}
+
+	public void setRm(int rm) {
+		this.rm = rm;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public byte[] getFoto() {
+		return foto;
+	}
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+
+	public boolean isBolsista() {
+		return bolsista;
+	}
+
+	public void setBolsista(boolean bolsista) {
+		this.bolsista = bolsista;
+	}
+
+	public int getSenhaAtendimento() {
+		return senhaAtendimento;
+	}
+
+	public void setSenhaAtendimento(int senhaAtendimento) {
+		this.senhaAtendimento = senhaAtendimento;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [rm=" + rm + ", nome=" + nome + ", cpf=" + cpf + ", dataNascimento=" + dataNascimento + ", sexo="
+				+ sexo + ", foto=" + Arrays.toString(foto) + ", bolsista=" + bolsista + ", senhaAtendimento="
+				+ senhaAtendimento + "]";
+	}
+	
+	
+	
 	
 }
